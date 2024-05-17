b673783caeb7860173aa7aadde0f42742d066356
rakesh ranjan kumar <rakeshranjan804432@gmail.com>
2024-05-17 23:14:35 +0530

    My Portfolio is Set-Up and git initialization is done for version/steps control'

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+alert("Your future will shine brightly if you approach this project with 100% consistency, authenticity, and a commitment to self-improvement.");
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <App/>
+)
